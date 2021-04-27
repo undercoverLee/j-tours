c76c1e2ef387d3a0a83d16f804c2afd615454c07
undercoverLee <vardonabytyqi3@gmail.com>
2021-04-27 19:23:01 +0200

    POSTMAN & Node App

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello from the sever side",
+        app: "jTours"
+    })
+})
+
+app.post('/', (req, res) => {
+    res.send("You can post to this URL")
+})
+
+
+const tours = JSON.parse(fs.readFileSync('${__dirname}/dev-data/data/tours-simple.js'))
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server is listeing")
+})
+
