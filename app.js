dfbe3e1f505a2d070df4b5626fdd8023d06dc74d
undercoverLee <vardonabytyqi3@gmail.com>
2021-04-29 19:13:45 +0200

    POST & GET

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs')
 
 const app = express()
 
+// middelware - mes req, res
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.json({
         message: "Hello from the sever side",
@@ -15,8 +18,35 @@ app.post('/', (req, res) => {
 })
 
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.js`))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
+app.get('/api/v1/tours', (req, res) => {
+    res.json({
+        status: "success",
+        data: { tours }
+    })
+})
+
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+
+    // ka me u shtu nje dokument i ri
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: newId }, req.body)
+
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        })
+
+    })
+
+})
 
 
 
